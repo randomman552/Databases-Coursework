1fe04ba70b6861ae1e6076ea07e5a09af4713ae6
George Grainger <random.man552@protonmail.com>
2022-01-02 16:00:07 +0000

    Re-organise file structure

---
+++ Questions/1/2/i/Database_A.js
@@ -0,0 +1,65 @@
+// TODO: This query is VERY slow
+db.users.aggregate([
+    {
+        '$lookup': {
+            'from': 'badges', 
+            'let': {
+            'id': '$id'
+            }, 
+            'as': 'badges', 
+            'pipeline': [
+            {
+                '$match': {
+                '$expr': {
+                    '$eq': [
+                    '$user_id', '$$id'
+                    ]
+                }
+                }
+            }, {
+                '$project': {
+                '_id': 0, 
+                'id': 0, 
+                'user_id': 0
+                }
+            }
+            ]
+        }
+    }, {
+        '$match': {
+            'badges': {
+            '$elemMatch': {
+                'name': 'Fanatic'
+            }
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'id': 1, 
+            'display_name': 1, 
+            'badges': {
+            '$filter': {
+                'input': '$badges', 
+                'as': 'badge', 
+                'cond': {
+                '$eq': [
+                    '$$badge.name', 'Fanatic'
+                ]
+                }
+            }
+            }
+        }
+    }, {
+        '$unwind': {
+            'path': '$badges'
+        }
+    }, {
+        '$project': {
+            'id': 1, 
+            'display_name': 1, 
+            'badge_name': '$badges.name', 
+            'date_awarded': '$badges.date'
+        }
+    }
+]);
---
+++ Questions/1/2/i/Database_B.js
@@ -0,0 +1,39 @@
+db.users.aggregate([
+    {
+        '$match': {
+            'badges': {
+                '$elemMatch': {
+                'name': 'Fanatic'
+                }
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'id': 1, 
+            'display_name': 1, 
+            'badges': {
+                '$filter': {
+                'input': '$badges', 
+                'as': 'badge', 
+                'cond': {
+                    '$eq': [
+                    '$$badge.name', 'Fanatic'
+                    ]
+                }
+                }
+            }
+        }
+    }, {
+        '$unwind': {
+            'path': '$badges'
+        }
+    }, {
+        '$project': {
+            'id': 1, 
+            'display_name': 1, 
+            'badge_name': '$badges.name', 
+            'date_awarded': '$badges.date'
+        }
+    }
+]);
